196a6872662a6bc37acc1ad970afcf2306548791
ambitionchaudhary <ambitionchaudhary123@gmail.com>
2018-11-09 00:22:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 21:27:23 2018
+
+@author: Ambition
+"""
+
+# linear algebra
+import numpy as np 
+
+# data processing
+import pandas as pd 
+
+# data visualization
+import seaborn as sns
+%matplotlib inline
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+# Algorithms
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
+
